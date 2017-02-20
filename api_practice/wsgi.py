d8f82d440b2dae520b2173164cd7ea9948ffff3b
cjredmond <connor.redmond@gmail.com>
2017-02-20 16:47:54 -0500

    fixed error in wsgi

--- api_practice/wsgi.py
+++ api_practice/wsgi.py
@@ -1,5 +1,5 @@
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_practice.settings")
