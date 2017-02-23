e1f59d697bfe0752b571f16a00de4e15fd35cee6
cjredmond <connor.redmond@gmail.com>
2017-02-23 11:14:40 -0500

    install cors headers

--- api_practice/settings.py
+++ api_practice/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'sample',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -37,8 +38,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True 
 ROOT_URLCONF = 'api_practice.urls'
 
 TEMPLATES = [
