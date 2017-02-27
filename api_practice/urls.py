12519d118bc133a8b60b2a94feef16b39deeefe1
cjredmond <connor.redmond@gmail.com>
2017-02-27 14:46:53 -0500

    catching up

--- api_practice/urls.py
+++ api_practice/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     url(r'^api/:persons/(?P<pk>\d+)/$', PersonDetailUpdateDestroyAPIView.as_view(), name="person_detail_api_view"),
     url(r'^api/:free-agents/$', FreeAgentListCreateAPIView.as_view(), name="free_agents_list_api_view"),
     url(r'^api/:free-agents/(?P<pk>\d+)/$', FreeAgentDetailUpdateDestroyAPIView.as_view(), name="free_agents_detail_api_view"),
-    url(r'^api/:packers-player/$', PackersPlayerListCreateAPIView.as_view(), name="packers_list_api_view"),
-    url(r'^api/:packers-player/(?P<pk>\d+)/$', PackersPlayerDetailUpdateDestroyAPIView.as_view(), name="packers_detail_api_view"),
+    url(r'^api/:packers-players/$', PackersPlayerListCreateAPIView.as_view(), name="packers_list_api_view"),
+    url(r'^api/:packers-players/(?P<pk>\d+)/$', PackersPlayerDetailUpdateDestroyAPIView.as_view(), name="packers_detail_api_view"),
 ]
